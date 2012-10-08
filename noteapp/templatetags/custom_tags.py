9a67db6c7914510f32e7aa0ff2c5750a6354c340
Dmitriy Bazas <babyx64@gmail.com>
2012-10-09 01:34:18 +0300

    Created template tag that render one text note by given id of note.

---
+++ noteapp/templatetags/custom_tags.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+from noteapp.models import *
+
+
+@register.simple_tag
+def render_one_text_note(number_id):
+	try:
+		t=Entry.objects.get(pk=number_id)
+	except Entry.DoesNotExist:
+		print "Error. Incorrect ID. Apress isn't in the database yet."
+		return ""
+	else:
+		return t.text.replace('\n', '</br>')
