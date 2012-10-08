f4270efc5f0088d3364dfefe2f284c9ce60c6e13
Dmitriy Bazas <babyx64@gmail.com>
2012-10-09 02:25:38 +0300

    Add ability, Length should be at least 10 characters of field Entry.text

--- noteapp/models.py
+++ noteapp/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 import datetime
 
+from django.core.validators import RegexValidator
+
 # Create your models here.
 
 class Entry(models.Model):
 	title = models.CharField(max_length=250)
-	description = models.TextField(blank=True)
-	text = models.TextField(blank=True)
+	description = models.TextField( blank=True)
+	text = models.TextField( validators=[RegexValidator(regex='^.{10,}?$', message='Length should be at least 10 characters!', code='nomatch')])
 	date = models.DateTimeField(auto_now=True)
 	def __unicode__(self):
 		return self.title
