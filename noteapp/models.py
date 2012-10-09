28b69b1bdd8f9fa244087850905e99b2d63d2f98
Dmitriy Bazas <babyx64@gmail.com>
2012-10-09 21:41:25 +0300

    create sqlite3 base and fix min length for field Enrty.text .

--- noteapp/models.py
+++ noteapp/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 import datetime
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator, MinLengthValidator
 
 
 class Entry(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField(blank=True)
-    text = models.TextField(validators=[RegexValidator(regex='^.{10,}?$',
-                            message='Length should be at least 10 characters!',
-                            code='nomatch')])
+    text = models.TextField(validators=[MinLengthValidator(10)])
     date = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
