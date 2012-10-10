4cfcc248f80e0cb3d81d65648e724768ad06497b
Dmitriy Bazas <babyx64@gmail.com>
2012-10-10 19:17:30 +0300

    add test for urls and for read/write database.

--- tnote/noteapp/tests.py
+++ tnote/noteapp/tests.py
@@ -14,3 +14,27 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+class MyTests(TestCase):
+    def test_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin(self):
+        response = self.client.get('/admin/')
+        self.assertEqual(response.status_code, 200)
+
+from django.utils import unittest
+from tnote.noteapp.models import Entry
+
+
+class EntryTestCase(unittest.TestCase):
+    def setUp(self):
+        self.one = Entry.objects.create(title="title_1", text="text_1",
+                                       date="2012-7-07 19:21:36.369778")
+        self.two = Entry.objects.create(title="title_2", text="text_2",
+                                       date="2012-8-07 19:21:36.369778")
+
+    def testInBase(self):
+        self.assertEqual(self.one.__unicode__(), 'title_1')
