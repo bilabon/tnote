cc3b0788b83f610d617c829426cb4c86b7efba0c
Dmitriy Bazas <babyx64@gmail.com>
2012-10-21 01:26:27 +0300

    add widget that Show total count of notes. add js

--- tnote/static/js/count_of_notes.js
+++ tnote/static/js/count_of_notes.js
@@ -1,6 +1,6 @@
 $(function() {
     $("textarea[id='id_text']").keyup(function count(){
     number = $("textarea[id='id_text']").val().length;
-    $("label[for='id_text']").html("Text("+number+"): ")
+    $("label[for='id_text']").html("Text: "+number)
         ;})
         ;})
