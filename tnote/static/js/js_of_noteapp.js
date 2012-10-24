56378abb3d5c53f25dd790368042a48e10a2d7c5
Dmitriy Bazas <babyx64@gmail.com>
2012-10-24 05:25:58 +0300

    add view for ajax form

--- tnote/static/js/js_of_noteapp.js
+++ tnote/static/js/js_of_noteapp.js
@@ -1,4 +1,5 @@
 $(function() {
+    $("label[for='id_text']").html("Text: 0")
     $("textarea[id='id_text']").keyup(function count(){
     number = $("textarea[id='id_text']").val().length;
     $("label[for='id_text']").html("Text: "+number)
