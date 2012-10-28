ff9713b7a4871ed53b6b1edf5e6f6920c089fbe7
Dmitriy Bazas <babyx64@gmail.com>
2012-10-28 07:02:33 +0200

    fix js

--- tnote/static/js/js_of_noteapp.js
+++ tnote/static/js/js_of_noteapp.js
@@ -1,5 +1,7 @@
 $(function() {
     $("label[for='id_text']").html("Text: 0");
+    $("textarea[id='id_text']").click();
+    $("label[for='id_text']").html("Text: " + $("textarea[id='id_text']").val().length);
     $("textarea[id='id_text']").bind('change click keyup', function count() {
         number = $("textarea[id='id_text']").val().length;
         $("label[for='id_text']").html("Text: " + number);
