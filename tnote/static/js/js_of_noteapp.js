4c618db03b35c97e98e048de65d985efd630bf82
Dmitriy Bazas <babyx64@gmail.com>
2012-10-28 17:35:52 +0200

    fix js

--- tnote/static/js/js_of_noteapp.js
+++ tnote/static/js/js_of_noteapp.js
@@ -5,16 +5,14 @@ $(function() {
     $("textarea[id='id_text']").bind('change click keyup', function count() {
         number = $("textarea[id='id_text']").val().length;
         $("label[for='id_text']").html("Text: " + number);
-    })
-    ;
-})
+    });
+});
 
 function tstFile(val) {
     var v = val.value;
-    if (v != 0) {
+    if (v !== false) {
         $('#pushimg').click();
     }
-
 }
 
 function display_form_errors(errors, $form) {
@@ -24,7 +22,6 @@ function display_form_errors(errors, $form) {
 }
 
 $(document).ready(function() {
-    
     $('#push_form').live('click', function() {
         var bar = $('.bar');
         var percent = $('.percent');
@@ -34,16 +31,18 @@ $(document).ready(function() {
             beforeSend: function() {
                 status.empty();
                 var percentVal = '0%';
-                bar.width(percentVal)
+                bar.width(percentVal);
                 percent.html(percentVal);
             },
             uploadProgress: function(event, position, total, percentComplete) {
                 var percentVal = percentComplete + '%';
-                bar.width(percentVal)
+                bar.width(percentVal);
                 percent.html(percentVal);
             },
             complete: function(xhr) {
                 status.html(xhr.responseText);
+                bar.width('100%');
+                percent.html('100%');
             },
             success: function(data, statusText, xhr, $form) {
                 // remove errors
@@ -51,10 +50,9 @@ $(document).ready(function() {
                 if (data['result'] == 'success') {
                     $('#id_text').val('');
                     //$('#Reset').click();
-                    $("label[for='id_text']").html("Text: 0")
-                    $form.find('.error_list').after('<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">×</button><strong>' + data['response'] + '</strong></div>');
-                
-                } 
+                    $("label[for='id_text']").html("Text: 0");
+                    $form.find('.error_list').after('<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">×</button><strong>' + data['response'] + '</strong></div>');                
+                }
                 else if (data['result'] == 'error') {
                     // display errors
                     display_form_errors(data['response'], $form);
@@ -76,22 +74,24 @@ $(document).ready(function() {
             beforeSend: function() {
                 status.empty();
                 var percentVal = '0%';
-                bar.width(percentVal)
+                bar.width(percentVal);
                 percent.html(percentVal);
             },
             uploadProgress: function(event, position, total, percentComplete) {
                 var percentVal = percentComplete + '%';
-                bar.width(percentVal)
+                bar.width(percentVal);
                 percent.html(percentVal);
             },
             complete: function(xhr) {
                 status.html(xhr.responseText);
+                bar.width('100%');
+                percent.html('100%');
             },
             success: function(data, statusText, xhr, $form) {
                 // remove errors
                 $form.find('.alert').remove();
                 if (data['result'] == 'success') {
-                    $('#id_text').val($('#id_text').val() + '<img src="..' + data['imagefileurl'] + '"class="img-polaroid" width="100" height="100" />');
+                    $('#id_text').val($('#id_text').val() + '<img src="..' + data['imagefileurl'] + '" class="img-polaroid" width="100" height="100" />');
                     $('#Resetimg').click();
                     $("textarea[id='id_text']").click();
                     $form.find('.error_list').after('<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">×</button><strong>' + data['response'] + '</strong></div>');
@@ -106,4 +106,5 @@ $(document).ready(function() {
     });
 //*-----------------------*/
 }
-)
+);
+
