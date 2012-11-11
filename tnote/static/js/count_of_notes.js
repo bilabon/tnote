4c88b0f4743c8cc46f98733e92e2c7ffc10ea76a
Dmitriy Bazas <babyx64@gmail.com>
2012-11-11 20:20:39 +0200

    fix templates and js

--- tnote/static/js/count_of_notes.js
+++ tnote/static/js/count_of_notes.js
@@ -1,6 +1,8 @@
 $(function() {
-    $("textarea[id='id_text']").keyup(function count(){
+    $("textarea[id='id_text']").click();
+    $("label[for='id_text']").html("Text: " + $("textarea[id='id_text']").val().length);
+    $("textarea[id='id_text']").bind('change click keyup', function count(){
     number = $("textarea[id='id_text']").val().length;
-    $("label[for='id_text']").html("Text: "+number)
-        ;})
-        ;})
+    $("label[for='id_text']").html("Text: "+number);
+    })
+})
