8da2e8815b0020afa66039aaf165e6454c9a494b
Dmitriy Bazas <babyx64@gmail.com>
2012-11-12 01:44:01 +0200

    change form to use ajax

--- tnote/static/js/count_of_notes.js
+++ tnote/static/js/count_of_notes.js
@@ -1,8 +1,10 @@
-$(function() {
-    $("textarea[id='id_text']").click();
-    $("label[for='id_text']").html("Text: " + $("textarea[id='id_text']").val().length);
-    $("textarea[id='id_text']").bind('change click keyup', function count(){
-    number = $("textarea[id='id_text']").val().length;
-    $("label[for='id_text']").html("Text: "+number);
+if (document.getElementById){
+    $(function() {
+        $("textarea[id='id_text']").click();
+        $("label[for='id_text']").html("Text: " + $("textarea[id='id_text']").val().length);
+        $("textarea[id='id_text']").bind('change click keyup', function count(){
+        number = $("textarea[id='id_text']").val().length;
+        $("label[for='id_text']").html("Text: "+number);
+        })
     })
-})
+}
