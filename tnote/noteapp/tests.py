19020c09fcabe1a618d7e3a26b828ef5abf2dff9
Dmitriy Bazas <babyx64@gmail.com>
2012-11-17 22:46:20 +0200

    add tests |add ability to add new text note

--- tnote/noteapp/tests.py
+++ tnote/noteapp/tests.py
@@ -67,3 +67,22 @@ class FormsWidgetsTestCase(TestCase):
         w = DynamicAmountOfSymbols(attrs={'rows': '50', 'cols': '50'})
         self.assertHTMLEqual(w.render('msg', ''),
                 '<textarea rows="50" cols="50" name="msg"></textarea>')
+
+
+class Forms_Submission_TestCase(TestCase):
+    def test_add_note_success(self):
+        _sometext = u'simple_text_12345'
+        response = self.client.post('/add/', {'text': _sometext})
+        self.assertTrue(len(_sometext) > 10)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Note was successfully added.', response.content)
+        self.assertTrue(Entry.objects.get(text=_sometext))
+        obj = Entry.objects.get(text=_sometext)
+        self.assertEqual(_sometext, obj.text)
+
+    def test_add_note_fail(self):
+        _sometext = u's_text'
+        response = self.client.post('/add/', {'text': _sometext})
+        self.assertFalse(len(_sometext) > 10)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Some error in your data.', response.content)
